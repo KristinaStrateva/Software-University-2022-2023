6447f54797008adaa28af9f2222364dc9189e1af
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-09-21 16:26:38 +0300

    Add ExpressJS to project

--- JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello ExpressJS!');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on posrt ${PORT}...`));
