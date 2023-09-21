55a1d5ae57cb50aa1b22ff113fa45fbbcc07dbb3
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-09-21 17:52:30 +0300

    Add hadlebars

--- JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = 5000;
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', '/src/views');
 
 app.get('/', (req, res) => {
     res.send('Hello ExpressJS!');
