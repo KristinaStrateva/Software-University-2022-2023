7094d46e76f0c70336ccd40b53be076f7d792aca
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-09-22 13:15:38 +0300

    Setting on handlebars

--- JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -4,13 +4,16 @@ const handlebars = require('express-handlebars');
 const app = express();
 const PORT = 5000;
 
+app.use(express.static('src/public'));
+
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
-app.set('view engine', '/src/views');
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
 
 app.get('/', (req, res) => {
-    res.send('Hello ExpressJS!');
+    res.render('index');
 });
 
 app.listen(PORT, () => console.log(`Server is listening on posrt ${PORT}...`));
