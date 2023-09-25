16cb32161135779bb086d23223f5a4d486cf1bc9
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-09-25 12:16:01 +0300

    Keep searched values to the inputs

--- JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/controllers/homeController.js
+++ JS-Back-End-Sep-2023/JS_Back-End/03. Cubicle-Workshop-Part-1-Resources/src/controllers/homeController.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
 
     const cubes = cubeManager.getAllCubes(search, from, to);
 
-    res.render('index', { cubes });
+    res.render('index', { cubes, search, from, to });
 });
 
 router.get('/about', (req, res) => {
