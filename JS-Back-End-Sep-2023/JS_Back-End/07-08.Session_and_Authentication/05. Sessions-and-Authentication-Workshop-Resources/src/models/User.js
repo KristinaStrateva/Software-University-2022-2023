3d03b7340a5c1278dd1ad98ff6c4f42a021efd2a
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-10-06 16:02:28 +0300

    Hashing the password before save

--- JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/models/User.js
+++ JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -26,7 +27,13 @@ userSchema.virtual('repeatPassword')
         if (value !== this.password) {
             throw new mongoose.MongooseError('Passwords don\'t match!');
         }
-    })
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+});
 
 const User = mongoose.model('User', userSchema);
 
