7d3878ec5d49e87aac248885d0667086f0fd7036
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-10-06 15:11:51 +0300

    Add register page

---
+++ JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/managers/userManager.js
@@ -0,0 +1,4 @@
+const User = require('../models/User');
+
+exports.register = (userData) => User.create(userData);
+
---
+++ JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                return value.length >= 6 && value.length <= 15;
+            },
+            message: 'Username must be 6 to 15 characters!',
+        }
+    },
+    password: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                return this.repeatPassword === value;
+            },
+            message: 'Passwords don\'t match!',
+        }
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
