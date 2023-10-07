e503fdbf89430596379559f38bbaf042b9bb7215
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-10-07 23:15:19 +0300

    User login

--- JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/controllers/userController.js
+++ JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/controllers/userController.js
@@ -5,19 +5,21 @@ const userManager = require('../managers/userManager');
 router.get('/register', (req, res) => res.render('users/register'));
 
 router.post('/register', async (req, res) => {
-    const {username, password, repeatPassword} = req.body;
+    const { username, password, repeatPassword } = req.body;
 
-    await userManager.register({username, password, repeatPassword});
+    await userManager.register({ username, password, repeatPassword });
 
     res.redirect('/users/login');
 });
 
 router.get('/login', (req, res) => res.render('users/login'));
 
-router.post('/login', (req, res) => {
-    const {username, password} = req.body;
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await userManager.login(username, password);
 
     res.redirect('/');
-})
+});
 
 module.exports = router;
--- JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/managers/userManager.js
+++ JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/managers/userManager.js
@@ -1,5 +1,19 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 exports.register = (userData) => User.create(userData);
 
-// exports.login = (userData) => 
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+    if (!user) {
+        throw new Error('Invalid username or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Invalid username or password!');
+    }
+
+    return user;
+};
