e93af83c094545f5a2dc421bc045b9ca5aa7a6fd
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-10-07 23:45:39 +0300

    Generate random secret code

--- JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/managers/userManager.js
+++ JS-Back-End-Sep-2023/JS_Back-End/07-08.Session_and_Authentication/05. Sessions-and-Authentication-Workshop-Resources/src/managers/userManager.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('../lib/jwt');
 
-const SECRET = 'thisismyveryverylongsecret';
+const SECRET = '9b42ee31d055fe0b122359ddad5e461ea32b340dc29734c1d0851cd4d0e9ad0e';
 
 exports.register = (userData) => User.create(userData);
 
