8f343f9adbc181e9f5f6e79417ee0fac5dcc095f
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-10-19 21:07:40 +0300

    Added Add photo page

---
+++ JS-Back-End-Sep-2023/JS_Back-End/Exam_Preparation/src/models/Photo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const photoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required!'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required!'],
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image URL is required!'],
+    },
+});
+
+const Photo = mongoose.model('Photo', photoSchema);
+
+module.exports = Photo;
