55c9fcde21158aed6fb99226df5fcc19a2dc9468
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-01 00:42:41 +0200

    Add conditional rendering to the counter

--- JS-Back-End-Sep-2023/ReactJS/02.Components-Basic_Idea/src/components/Counter.jsx
+++ JS-Back-End-Sep-2023/ReactJS/02.Components-Basic_Idea/src/components/Counter.jsx
@@ -15,12 +15,41 @@ export default function Counter(props) {
         setCount(oldValue => oldValue + 1);
     }
 
+    // Conditional rendering with return
+
+    // if (count < 0) {
+    //     return (
+    //         <h3>Invalid counter!</h3>
+    //     );
+    // }
+
+    // Conditional rendering with if
+
+    // let warning = null;
+
+    // if (count < 0) {
+    //     warning = <p>Invalid counter!</p>
+    // }
+
     return (
         <div>
 
             <h3>Counter</h3>
 
             <p>{count}</p>
+
+            {/* {warning} */}
+
+            {/* Conditional rendering with ternary 
+
+                {count < 0
+                ? <p>Invalid counter!</p>
+                : null
+                } */}
+
+            {/* Conditional rendering with boolean */}
+
+            {count < 0 && <p>Invalid counter!</p>}  {/* If count is less than 0 only then show the 'Invalid counter!' message */}
 
             <button onClick={onDecrementClick}>-</button>
             <button onClick={resetCounter}>Clear</button>
