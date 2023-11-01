d30bc77321dd7b0f526bd13d2d3c91271f93689c
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2023-11-01 15:49:37 +0200

    Add some useEffect hooks and add css to the App component

--- JS-Back-End-Sep-2023/ReactJS/03.Components-Deep_Dive/src/App.jsx
+++ JS-Back-End-Sep-2023/ReactJS/03.Components-Deep_Dive/src/App.jsx
@@ -1,13 +1,44 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 function App() {
-    const numbers = [1, 2, 3, 4, 5, 6];
-    const doubled = numbers.map(num => <li key={num}>{num * 2}</li>);
+    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6]);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log('Mount component')
+    }, []);
+
+    useEffect(() => {
+        console.log(`Update component - ${numbers.length}`);
+    }, [numbers]);
+
+    useEffect(() => {
+        setTimeout(() => setCount(c => c + 1), 1000);
+    }, [count]); // it will work incorrectly if the + button is clicked
+
+    const onClick = () => {
+        setNumbers(oldState => oldState.slice(0, oldState.length - 1));
+    }
 
     return (
-        <ul>
-            {doubled}
-        </ul>
+        <div className={styles.app}>
+            <h3>Counter: {count}</h3>
+
+            <ul>
+                {numbers.map((num, index) => (
+                    <li
+                        key={index}
+                        className={styles['list-item']}
+                    >
+                        {num * 2}
+                    </li>)
+                )}
+            </ul>
+
+            <button style={{marginRight: '10px'}} onClick={onClick}>Remove</button>
+            <button onClick={() => setCount(c => c + 1)}>+</button>
+        </div>
     );
 }
 
